ce4da32dd8ad4877ab2c3389b08b32ed2df5e210
malachaneG <g.malachane@sciencesu-lyon.fr>
2021-05-01 20:48:36 +0200

    2ème commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'cj654063-002.dbaas.ovh.net',//cj654063-001.privatesql
-  user     : 'gsbmalachane',//gsbmalachane
-  password : 'gsbmalachaneSU2020',//gsbmalachaneSU2020
-  database : 'gsb'//bdgsb
+  host     : 'cj654063-002.dbaas.ovh.net',//cj654063-002.dbaas.ovh.net / 127.0.0.1
+  port     : '35305',
+  user     : 'gsbmalachane',//gsbmalachane / root
+  password : 'gsbmalachaneSU2020',//gsbmalachaneSU2020 / root
+  database : 'gsbmalachane'//bdgsb
 });
 
 module.exports = connection
